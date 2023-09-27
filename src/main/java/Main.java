f206847c9e01c8165b87aa6c31062ce584d2b871
joffroy8u <theo.joffroy7@etu.univ-lorraine.fr>
2023-09-27 19:00:19 +0200

    Ajout du Main qui n'était pas inclus dans le précédent commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import model.PacmanPainter;
+import engine.GameEngineGraphical;
+import model.PacmanController;
+import model.PacmanGame;
+
+/**
+ * lancement du moteur avec le jeu
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("sqdfsd");
-    }
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// creation du jeu particulier et de son afficheur
+		PacmanGame game = new PacmanGame("helpFilePacman.txt");
+		PacmanPainter painter = new PacmanPainter();
+		PacmanController controller = new PacmanController();
+
+		// classe qui lance le moteur de jeu generique
+		GameEngineGraphical engine = new GameEngineGraphical(game, painter, controller);
+		engine.run();
+	}
+
 }
