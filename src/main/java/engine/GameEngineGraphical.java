6799bec9b9719bea2e6eac0441c314d99b48dcfe
tojry <theojoffroy@gmail.com>
2023-10-12 19:45:08 +0200

    Mise en commentaire du message de debug des FPS

--- src/main/java/engine/GameEngineGraphical.java
+++ src/main/java/engine/GameEngineGraphical.java
@@ -98,7 +98,7 @@ public class GameEngineGraphical {
 
 			if (System.currentTimeMillis() - lastTimer >= 1000) {
 				lastTimer += 1000;
-				System.out.println(ticks + " ticks, " + frames + " frames");
+				//System.out.println(ticks + " ticks, " + frames + " frames");
 				frames = 0;
 				ticks = 0;
 			}
