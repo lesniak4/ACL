6ba8be899dcff4b9723a79d3241950896d33ee95
Supradom <55812171+Supradom@users.noreply.github.com>
2023-12-12 12:55:08 +0100

    Timer fonctionnel, besoin de vérifier quand le joueur quitte la case apprentissage.

--- src/main/java/model/components/world/SwimmingLessonComponent.java
+++ src/main/java/model/components/world/SwimmingLessonComponent.java
@@ -2,26 +2,53 @@ package model.components.world;
 
 import model.GameObject;
 import model.components.Component;
+import model.components.physics.PlayerMovementComponent;
+import utils.GameConfig;
+
+import java.util.*;
 
 public class SwimmingLessonComponent extends Component {
     private boolean currentlyLearning;
+    private boolean finishedLearning;
 
     public SwimmingLessonComponent(GameObject obj) {
         super(obj);
 
         this.currentlyLearning = false;
+        this.finishedLearning = false;
     }
 
     public void startLearning() {
         this.currentlyLearning = true;
+        learningCheck();
+    }
+
+    public void stopLearning() {
+        this.currentlyLearning = false;
+    }
+
+    public void learningCheck() {
+        Timer lesson = new Timer(false);
+        lesson.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(isCurrentlyLearning()) {
+                    setFinishedLearning(true);
+                }
+            }
+        }, GameConfig.getInstance().getTimeNeededToLearn());
     }
 
     public boolean isCurrentlyLearning() {
         return currentlyLearning;
     }
 
+    public void setFinishedLearning(boolean hasLearnt) {
+        this.finishedLearning = hasLearnt;
+    }
+
     public boolean finishedLearning() {
-        return true;
+        return this.finishedLearning;
     }
 
     @Override
